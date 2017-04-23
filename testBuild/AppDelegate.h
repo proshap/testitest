6bddfcfaf5522a2c1a27888c2a009c7644f306ff
paygilant <paygilant@paygilants-macbook-pro.local>
2017-04-23 11:51:48 +0300

    Initial Commit

---
+++ testBuild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testBuild
+//
+//  Created by paygilant on 23/04/2017.
+//  Copyright © 2017 paygilant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testBuild/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testBuild
+//
+//  Created by paygilant on 23/04/2017.
+//  Copyright © 2017 paygilant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
